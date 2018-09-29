cea41967b8881627a44a436c7dde7a584c5828c1
vishalbeppurana <vishalbep6@gmail.com>
2018-09-29 12:47:34 +0530

    Pointers included

---
+++ Pointers.pro
@@ -0,0 +1,11 @@
+TEMPLATE = app
+CONFIG += console c++11
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += main.cpp \
+    callbyaddress.cpp \
+    Arrayasfunctionarguments.cpp \
+    characterarray.cpp \
+    Pointers2Dimensional.cpp \
+    SwapwithPointers.cpp
---
+++ main.cpp
@@ -0,0 +1,33 @@
+//#include <iostream>
+//using namespace std;
+//int add(int a,int b)
+//{
+//    cout<<"The address  of A in add():"<<&a<<endl;
+//    int c=a+b;
+//    return c;
+//}
+//int main()
+//{
+//    int a=2,b=3;
+
+//    cout<<add(a,b);
+//    cout<<"The address  of A in main():"<<&a<<endl;
+//    return 0;
+//}
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//    int x=5;
+//    int *p;
+//    p=&x;
+//    *p=6;
+//    int **q=&p;
+//    int ***r=&q;
+//    cout<<"Value of *p :"<<*p<<endl;
+//    cout<<"Address of *p:"<<p<<endl;
+//    cout<<"Value of **q :"<<**q<<endl;
+//    cout<<"Address of **q:"<<q<<endl;
+//    cout<<"Value of ***r :"<<***r<<endl;
+//    cout<<"Address of ***r:"<<r<<endl;
+//}
