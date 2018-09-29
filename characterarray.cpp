aecacaab476798394cb168ba39031a7d78fa21c2
vishalbeppurana <vishalbep6@gmail.com>
2018-09-29 13:55:37 +0530

    Pointers included

--- characterarray.cpp
+++ characterarray.cpp
@@ -19,52 +19,52 @@
 //     printstring(c);
 
 //}
-#include<iostream>
-using namespace std;
-int add(int a,int b)
-{
-    int c;
-    c=a+b;
-    return c;
+//#include<iostream>
+//using namespace std;
+//int add(int a,int b)
+//{
+//    int c;
+//    c=a+b;
+//    return c;
 
-}
-int sub(int a,int b)
-{
-    int c;
-    c=a-b;
-    return c;
+//}
+//int sub(int a,int b)
+//{
+//    int c;
+//    c=a-b;
+//    return c;
 
-}
-int mul(int a,int b)
-{
-    int c;
-    c=a*b;
-    return c;
+//}
+//int mul(int a,int b)
+//{
+//    int c;
+//    c=a*b;
+//    return c;
 
-}
-int main()
-{
-    int num1,num2,ch;
-    cout<<"Enter the two numbers:"<<endl;
-    cin>>num1>>num2;
-    cout<<"Enter the choice:"<<endl;
-    cin>>ch;
-    switch(ch)
-    {
-        case 1:add(num1,num2);
-               cout<<"Addition of two numbers:"<<add(num1,num2)<<endl;
-               break;
-        case 2:sub(num1,num2);
-               cout<<"Subraction of two numbers:"<<sub(num1,num2)<<endl;
-               break;
-        case 3:mul(num1,num2);
-           cout<<"Multiplication of two numbers:"<<mul(num1,num2)<<endl;
-           break;
-        default:cout<<"Invalid"<<endl;
-               break;
-    }
+//}
+//int main()
+//{
+//    int num1,num2,ch;
+//    cout<<"Enter the two numbers:"<<endl;
+//    cin>>num1>>num2;
+//    cout<<"Enter the choice:"<<endl;
+//    cin>>ch;
+//    switch(ch)
+//    {
+//        case 1:add(num1,num2);
+//               cout<<"Addition of two numbers:"<<add(num1,num2)<<endl;
+//               break;
+//        case 2:sub(num1,num2);
+//               cout<<"Subraction of two numbers:"<<sub(num1,num2)<<endl;
+//               break;
+//        case 3:mul(num1,num2);
+//           cout<<"Multiplication of two numbers:"<<mul(num1,num2)<<endl;
+//           break;
+//        default:cout<<"Invalid"<<endl;
+//               break;
+//    }
 
-    //add(num1,num2);
-    return 0;
+//    //add(num1,num2);
+//    return 0;
 
-}
+//}
